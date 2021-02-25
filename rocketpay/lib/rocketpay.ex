6fd7035ca98608696df4930b4c4e151ebb3d9a5a
kazukig <kazukigushiken20@gmail.com>
2021-02-24 22:37:04 -0300

    dia 3

--- rocketpay/lib/rocketpay.ex
+++ rocketpay/lib/rocketpay.ex
@@ -2,4 +2,6 @@ defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
 
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit(params), to: Deposit, as: :call
 end
--- rocketpay/lib/rocketpay_web/controllers/users_controller.ex
+++ rocketpay/lib/rocketpay_web/controllers/users_controller.ex
@@ -1,25 +1,15 @@
 defmodule RocketpayWeb.UsersController do
   use RocketpayWeb, :controller
 
-  alias Rocketpay.User
+    alias Rocketpay.User
 
-  def create(conn, params) do
-    params
-    |> IO.inspect()
-    |> Rocketpay.create_user()
-    |> handle_response(conn)
-  end
+    action_fallback RocketpayWeb.FallbackController
 
-  defp handle_response({:ok, %User{} = user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render("create.json", user: user)
-  end
-
-  defp handle_response({:error, result}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(RocketpayWeb.ErrorView)
-    |> render("400.json", result: result)
+    def create(conn, params) do
+      with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
+        conn
+        |> put_status(:created)
+        |> render("create.json", user: user)
+    end
   end
 end
